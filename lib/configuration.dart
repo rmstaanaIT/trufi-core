06b6d82c1424bfcf196432d49755cd5393cd5bdf
Samuel <samuel@MacBook-Pro-de-Samuel.local>
2019-06-17 11:21:18 -0400

    change url

--- lib/configuration.dart
+++ lib/configuration.dart
@@ -4,8 +4,8 @@ const urlOtpEndpoint = 'trufiapp.westeurope.cloudapp.azure.com:8290';
 const urlOpenStreetMap = 'https://www.openstreetmap.org/copyright';
 const urlRouteFeedback = "https://trufifeedback.z15.web.core.windows.net/route.html";
 const urlTrufi = 'https://www.trufi.app';
-const urlFacebook = 'https://www.facebook.com/trufiapp';
-const urlInstagram = 'https://www.instagram.com/trufi.app';
+const urlFacebook = 'https://www.facebook.com/trotroapp1';
+const urlInstagram = 'https://twitter.com/trotroapp';
 
 final abbreviations = {
   "Avenida": "Av.",
