4e919d3cd8711999634705a0af20e0c6c8b0b793
rmstaanait <runamitchie@gmail.com>
2020-07-01 05:11:15 -0400

    Remove duplicated directory

--- test_driver/home.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_driver/driver_extension.dart';
-
-import 'package:jeepney_app/main.dart';
-
-void main() {
-  enableFlutterDriverExtension();
-  runApp(TrufiApp());
-}
